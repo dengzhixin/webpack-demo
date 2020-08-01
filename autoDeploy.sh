4f0669334d6b71761fd542d13e948261420f041b
deng <786339231@qq.com>
2020-08-01 16:19:35 +0800

    autoDeploy

---
+++ autoDeploy.sh
@@ -0,0 +1,10 @@
+yarn build &&
+git checkout gh_pages &&
+rm -rf *.html *.js *.json *.css *.png &&
+mv dist/* ./ &&
+rm -rg dist ;
+git add . &&
+git commit -m 'autoDeploy' &&
+git push
+git checkout -
+
