84e94fa6e236e60903b7e31cb47efee27b0fb921
deng <786339231@qq.com>
2020-08-01 13:26:52 +0800

    不同环境使用不同配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: '[name].[contentHash].js'
@@ -12,26 +11,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: "Webpack App",
             template: "./src/assets/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contentHash].css',
-            chunkFilename: '[id].[contentHash].css',
         })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../',
-                        },
-                    },
-                    'css-loader',
-                ],
-                // use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader'],
             },
         ],
     },
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,9 @@
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contentHash].js'
@@ -11,13 +12,26 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: "Webpack App",
             template: "./src/assets/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contentHash].css',
+            chunkFilename: '[id].[contentHash].css',
         })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                        },
+                    },
+                    'css-loader',
+                ],
+                // use: ['style-loader', 'css-loader'],
             },
         ],
     },
