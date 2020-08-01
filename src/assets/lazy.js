996c50bd1d49ce8c3520ac45bdfad85890c3dff1
deng <786339231@qq.com>
2020-08-01 15:31:07 +0800

    引入sass less stylus 引入图片 引入懒加载

---
+++ src/assets/lazy.js
@@ -0,0 +1,4 @@
+function lazy() {
+    console.log("我是懒加载的")
+}
+export default lazy
--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './assets/style.styl'
 
 import png from './assets/images/plus.png'
 console.log('hi')
-let img =document.querySelector("#img");
+let img =document.querySelector("#img")
 img.src=png
+
+const btn = document.createElement('button')
+btn.innerText='懒加载'
+
+btn.onclick=()=>{
+    const lazy = import('./assets/lazy.js')
+    lazy.then((module)=>{
+       let fn =  module.default
+        fn()
+    })
+}
+document.body.appendChild(btn)
